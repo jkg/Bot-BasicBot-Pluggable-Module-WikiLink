cc11a8ed8cd4d593985624e9f80fb3241a7e523d
James Green <jkg@earth.li>
2014-08-16 10:35:40 +0100

    bump version, update Changes, ready for release

--- lib/Bot/BasicBot/Pluggable/Module/WikiLink.pm
+++ lib/Bot/BasicBot/Pluggable/Module/WikiLink.pm
@@ -14,11 +14,11 @@ Bot::BasicBot::Pluggable::Module::WikiLink - a simple Wikipedia helper plugin fo
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
